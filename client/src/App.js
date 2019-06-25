c4b29c64a64bc4495dc088a3a6823aafa982bf93
BureikuT <bureikut@hotmail.com>
2019-06-24 20:03:53 -0600

    when registering, the user will now be authenticated and have their user info brought to the frontend through redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Components/auth/Login";
 import Navbar from "./Components/layout/Navbar";
@@ -6,26 +6,38 @@ import Register from "./Components/auth/Register";
 import Landing from "./Components/layout/Landing";
 import Alert from "./Components/layout/Alert";
 import { Provider } from "react-redux";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 import store from "./store";
 
 import "./App.css";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
