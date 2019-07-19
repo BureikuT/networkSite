e354b0e95b2f4b3991059fcd9bc7c821ff52ae87
BureikuT <bureikut@hotmail.com>
2019-07-18 19:24:48 -0600

    Links to specific profiles will now shoe user information on their own profile. If logged in, they'll be able to see the 'edit profile' button

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import CreateProfile from "./Components/profile-forms/CreateProfile";
 import AddExperience from "./Components/profile-forms/AddExperience";
 import AddEducation from "./Components/profile-forms/AddEducation";
 import Profiles from "./Components/profiles/Profiles";
+import Profile from "./Components/profile/Profile";
 import EditProfile from "./Components/profile-forms/EditProfile";
 import store from "./store";
 
@@ -41,6 +42,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
