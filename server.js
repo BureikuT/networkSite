96bb1bebdfdbc3276b15854ccc438a0696841980
BureikuT <bureikut@hotmail.com>
2019-08-27 20:22:07 -0600

    prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 
 const app = express();
@@ -10,13 +11,21 @@ connectDB();
 // Init Middleware
 app.use(express.json({extended: false}))
 
- app.get("/", (req, res) => res.send("Hello!"));
+
 
  // Use Controllers 
 app.use('/api/users', require('./server/controllers/users'))
 app.use('/api/profile', require('./server/controllers/profile'))
 app.use('/api/posts', require('./server/controllers/posts'))
 app.use('/api/auth', require('./server/controllers/auth'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
  const port = process.env.PORT || 5000;
 
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -69,7 +69,6 @@ router.post(
       console.error(err.message);
       res.status(500).send("Server error");
     }
-    console.log(req.body);
   }
 );
 
